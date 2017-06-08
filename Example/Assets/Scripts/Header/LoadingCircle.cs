8959b01d0c0c518b1ce242f6a638f3f2e9bea387
n1troo <n1troov4@gmail.com>
2017-06-08 16:19:00 +0200

    Circle added

---
+++ Example/Assets/Scripts/Header/LoadingCircle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingCircle : MonoBehaviour
+{
+    public Text ClockName;
+    public Text ClockValue;
+    public Color ClockProggresColor;
+    public Image CircleImageComponent;
+
+    private void Start()
+    {
+        CircleImageComponent.color = ClockProggresColor;
+    }
+
+    public void SetNameCircle(string valueName)
+    {
+        ClockName.text = valueName;
+    }
+    public void SetAmount(int value, int maximumValue)
+    {
+
+    }
+}
