795cf412364d167c027da534bde821b15050eb67
n1troo <n1troov4@gmail.com>
2017-06-20 14:19:52 +0200

    addedAudioSource when finished is rest time

--- Example/Assets/Scripts/BoxScripts/TimerScript.cs
+++ Example/Assets/Scripts/BoxScripts/TimerScript.cs
@@ -7,10 +7,18 @@ using UnityEngine.UI;
 public class TimerScript : AutoReferencer<TimerScript>
 {
     public Slider SliderTimer;
-    public event EventHandler FireEvent;
-    float timer = 0;
+    public event EventHandler FireEvent; 
     public int RestTimeSec { get; set; }
     public Text TextValue;
+    public AudioClip audioClipFinish;
+    AudioSource audioSource;
+
+    float timer = 0;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     IEnumerator TimerStart(GameObject localResult)
     {
@@ -26,8 +34,9 @@ public class TimerScript : AutoReferencer<TimerScript>
 
             if (SliderTimer.value + 1 > RestTimeSec)
             {
-                
+                //Tick row is done
                 localResult.gameObject.GetComponent<Image>().color = Color.blue;
+                localResult.gameObject.GetComponent<Image>().enabled = true;
                 ChangeStatusThread();
                 yield return new WaitForSeconds(1.0f);
                 SliderTimer.value = 0;
@@ -44,6 +53,11 @@ public class TimerScript : AutoReferencer<TimerScript>
     {
         if (FireEvent != null)
             FireEvent(this, null);
+
+        Debug.Log("start played");
+        audioSource.Play();
+        Debug.Log("end played");
+
     }
 
     public void StartTimer(GameObject localResult, DBTranning ss)
